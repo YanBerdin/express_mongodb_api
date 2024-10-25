cb8bd2016d6d8c0f6e31f9480edb0762c32c8934
YanBerdin <115979885+YanBerdin@users.noreply.github.com>
2024-10-25 00:44:17 +0000

    create data + routing get posts and create posts

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//const port = 3000
+const port = process.env.PORT || 3000;
+const data = require("./posts");
+
+let posts = [];
 
 app.get('/', (req, res) => {
   res.send('Hello Backend World!')
 })
 
+app.get("/posts", (req, res) => {
+    const data_posts = [...posts, ...data];
+    res.send(data_posts);
+  });
+  
+
+app.post("/posts/create", (req, res) => {
+    posts = [...posts, ...data, ...req.body];
+    res.send("new post successfully added");
+  });
+  
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
