a13ec7362122de0ed6774b5bd5aa223bbecf87a0
YanBerdin <yandevformation@gmail.com>
2024-10-28 18:26:32 +0100

    feat  Middleware loginRequired

--- src/protected_routes/protectedposts.js
+++ src/protected_routes/protectedposts.js
@@ -4,6 +4,16 @@ const {
   findOnePostAndDelete,
 } = require("../controllers/post");
 
+// Middleware
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    res.status(401).json({
+      message: "Unauthorized user, Please register a new account or login",
+    });
+  }
+  next();
+}
+
 module.exports = (protectedrouter) => {
   protectedrouter.post("/posts/create", createPost);
   protectedrouter.patch("/posts/update/:id", findOnePostAndUpdate);
