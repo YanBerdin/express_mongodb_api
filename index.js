4d6231d8e02b01dde87dd172186c0477a5985538
YanBerdin <yandevformation@gmail.com>
2024-10-28 18:13:07 +0100

    feat middleware to authorize protectedroutes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const app = express();
 //const Post = require("./models");
 const port = process.env.port || 3000;
@@ -16,122 +17,42 @@ require("./src/routes/freeposts")(postrouter);
 //! require("./src/routes/post")(postrouter);
 // require("./src/routes/userroutes")(router);
 require("./src/routes/userroutes")(userrouter);
+// require("./src/routes/post")(router);
 require("./src/protected_routes/protectedposts")(protectedrouter);
-
-
-
-
-
-// require("./src/protected_routes/post")(protectedrouter);
-
 
 app.use(express.json());
 
-// let posts = [];
-
-/** Without routeur */
-/*
-app.get("/", (req, res) => {
-  res.send("Hello Backend Express World!");
-});
-*/
+// Middleware
+function authorize(req, res, next) {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      process.env.TOKEN_SECRET,
+      function (err, decode) {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+}
 
 // Définir une route GET pour la racine
 postrouter.get("/", (req, res) => {
   res.send("Hello Backend Express World!");
 });
 
-/** Get local data */
-/*
-app.get("/posts", (req, res) => {
-    const data_posts = [...posts, ...data];
-    res.send(data_posts);
-  });
-*/
-
-/** Get data from database */
-/*
-app.get("/posts", async (req, res) => {
-  try {
-    const posts = await Post.find({});
-    res.send(posts);
-  } catch (error) {
-    res.send(error);
-  }
-});
-*/
-
-/** Get local data */
-/*
-app.post("/posts/create", (req, res) => {
-    // posts = [...posts, ...data, ...req.body];
-    posts = [...posts, req.body];
-    res.send("new post successfully added");
-  });
-  */
-
-/** data from database */
-/*
-app.post("/posts/create", (req, res) => {
-  try {
-    const post = new Post(req.body);
-    post.save();
-    res.send("new post successfully added");
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-*/
-
-/*
-app.put("/posts/update/:id", async (req, res) => {
-  try {
-    const query = { _id: req.params.id };
-    const update = req.body;
-    const post = await Post.findOneAndUpdate(query, update);
-    post.save();
-    res.send("new post successfully edited");
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-*/
-
-/*
-app.delete("/posts/delete/:id", async (req, res) => {
-  try {
-    const query = { _id: req.params.id };
-    const post = await Post.findOneAndDelete(query);
-    res.send("new post successfully deleted");
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-*/
-
-/* Local data */
-/*
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-*/
-
-/* Database data without routeur */
-/*
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-  mongoose.connect("mongodb://127.0.0.1:27017/database").then(() => {
-    console.log("successfully connected to the database !");
-  });
-});
-*/
-
 // Utiliser le routeur
 app.use("/", postrouter);
 app.use("/", userrouter);
+app.use(authorize);
 app.use("/", protectedrouter);
 
 // Démarrer le serveur
